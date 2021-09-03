87467a89137b46ee757bf095a72a0be12e2e100c
dsjlkw <dsjlkw@gztrc.edu.cn>
2021-09-03 00:14:39 +0000

    Initial commit

--- post-build.sh
+++ post-build.sh
@@ -10,7 +10,15 @@ OLD_CWD="$(pwd)"
 mkdir ~/imb && \
 tar -xJf bin/targets/ramips/mt76x8/openwrt-imagebuilder-${_version}-ramips-mt76x8.Linux-x86_64.tar.xz -C ~/imb && \
 cd ~/imb/* && \
-make image PROFILE=unielec_u7628-01-16m FILES=files/ PACKAGES="kmod-usb-storage block-mount kmod-fs-ext4 luci luci-proto-qmi kmod-usb-serial kmod-usb-serial-option kmod-usb-serial-wwan kmod-usb-uhci" && \
+mkdir -p files/etc/opkg/ && \
+echo src/gz openwrt_core http://downloads.openwrt.org/releases/${_version}/targets/ramips/mt76x8/packages > files/etc/opkg/distfeeds.conf && \
+echo src/gz openwrt_base http://downloads.openwrt.org/releases/${_version}/packages/mipsel_24kc/base >> files/etc/opkg/distfeeds.conf && \
+echo src/gz openwrt_luci http://downloads.openwrt.org/releases/${_version}/packages/mipsel_24kc/luci >> files/etc/opkg/distfeeds.conf && \
+echo src/gz openwrt_packages http://downloads.openwrt.org/releases/${_version}/packages/mipsel_24kc/packages >> files/etc/opkg/distfeeds.conf && \
+echo src/gz openwrt_routing http://downloads.openwrt.org/releases/${_version}/packages/mipsel_24kc/routing >> files/etc/opkg/distfeeds.conf && \
+echo src/gz openwrt_telephony http://downloads.openwrt.org/releases/${_version}/packages/mipsel_24kc/telephony >> files/etc/opkg/distfeeds.conf && \
+cp -r $GITHUB_WORKSPACE/files/* files/ && \
+make image PROFILE=unielec_u7628-01-16m FILES=files/ PACKAGES="kmod-usb-storage block-mount kmod-fs-ext4 luci luci-proto-qmi kmod-usb-serial kmod-usb-serial-option kmod-usb-serial-wwan kmod-usb-uhci kmod-usb-storage-uas kmod-usb-storage-extras luci-i18n-base-zh-cn" FILES=files && \
 mv bin/targets/ramips/mt76x8/openwrt-${_version}-ramips-mt76x8-unielec_u7628-01-16m-squashfs-sysupgrade.bin ../ && \
 make clean && \
 mv ../*.bin "$OLD_CWD/bin/targets/ramips/mt76x8/"
