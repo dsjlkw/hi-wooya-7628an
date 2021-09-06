1aa6e90aaa485c40a890ff1a478072865cdaf57f
dsjlkw <dsjlkw@gztrc.edu.cn>
2021-09-06 22:32:41 +0800

    Initial commit

--- pre-configure.sh
+++ pre-configure.sh
@@ -11,7 +11,7 @@ set -e -x
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-curl --retry 5 -L "https://downloads.openwrt.org/releases/21.02.0-rc4/targets/ramips/mt76x8/config.buildinfo" > .config
+curl --retry 5 -L "https://downloads.openwrt.org/releases/$REPO_BRANCH/targets/ramips/mt76x8/config.buildinfo" > .config
 sed -e '/^CONFIG_TARGET_DEVICE_/d' -e '/CONFIG_TARGET_MULTI_PROFILE=y/d' -i .config
 cat "$GITHUB_WORKSPACE/additional_config.txt" >> .config
 cat "$GITHUB_WORKSPACE/hi-wooya-7628an.dtsi" > target/linux/ramips/dts/mt7628an_unielec_u7628-01.dtsi
