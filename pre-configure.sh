13217706689f098a2b8ebea6e69c03b0b7d1ef89
dsjlkw <75516252+dsjlkw@users.noreply.github.com>
2021-10-18 20:10:16 +0800

    switch to v21.02.0

--- pre-configure.sh
+++ pre-configure.sh
@@ -11,8 +11,8 @@ set -e -x
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-curl --retry 5 -L "https://downloads.openwrt.org/releases/$REPO_BRANCH/targets/ramips/mt76x8/config.buildinfo" > .config
-sed -e '/^CONFIG_TARGET_DEVICE_/d' -e '/CONFIG_TARGET_ALL_PROFILES=y/d' -i .config
+curl --retry 5 -L "https://downloads.openwrt.org/releases/$(printf "%s" "$REPO_BRANCH" | cut -c 2-)/targets/ramips/mt76x8/config.buildinfo" > .config
+sed -e '/^CONFIG_TARGET_DEVICE_/d' -e '/CONFIG_TARGET_MULTI_PROFILE=y/d' -i .config
 cat "$GITHUB_WORKSPACE/additional_config.txt" >> .config
 cat "$GITHUB_WORKSPACE/hi-wooya-7628an.dtsi" > target/linux/ramips/dts/mt7628an_unielec_u7628-01.dtsi
 cp -r $GITHUB_WORKSPACE/package/* package/
